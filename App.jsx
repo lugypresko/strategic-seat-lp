64369df743eb6f88c6814de4b3a52fdc44978b9b
lugypresko <fitay@yahoo.com>
2025-09-04 11:43:47 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,6 @@
 // Add to your App component
 import { useEffect } from 'react';
-import { initGA, trackEvent } from './lib/analytics';
 
 export default function App() {
-  // Analytics temporarily disabled — will be added after deployment
-  
   // Rest of your component
 }
